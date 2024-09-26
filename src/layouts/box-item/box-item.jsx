61b912e0f0699efe09583855909ba077aa9110d4
Mint Rosetta <150432237+mintrosetta@users.noreply.github.com>
2024-09-26 22:15:37 +0700

    box-item

--- src/layouts/box-item/box-item.jsx
+++ src/layouts/box-item/box-item.jsx
@@ -1,4 +1,4 @@
-import './box-item.module.css';
+import styles from './box-item.module.css';
 
 export default function BoxItem() {
     return (
@@ -6,28 +6,31 @@ export default function BoxItem() {
             <div className='mt-3'>
                 <div>
                     <h2>2024-06</h2>
+                    <div className='d-flex justify-content-end'>
+                        <button className='btn btn-gold'>save</button>
+                    </div>
                     <hr />
                 </div>
-                <div>
-                    <div>
+                <div className='d-flex gap-3 mb-5'>
+                    <div className={styles.totalBox}>
                         <p>Income total</p>
                         <h2>24750</h2>
                     </div>
-                    <div>
+                    <div className={styles.totalBox}>
                         <p>Expense total</p>
                         <h2>12900</h2>
                     </div>
-                    <div>
+                    <div className={styles.totalBox}>
                         <p>Remaining</p>
                         <h2>11850</h2>
                     </div>
                 </div>
-                <div>
-                    <div>
+                <div className='row'>
+                    <div className='col-6'>
                         <div>
-                            <span>Incomes</span>
+                            <p>Incomes</p>
                         </div>
-                        <table className='table'>
+                        <table className='table table-dark table-striped'>
                             <thead>
                                 <tr>
                                     <th>Title</th>
@@ -36,6 +39,17 @@ export default function BoxItem() {
                                 </tr>
                             </thead>
                             <tbody>
+                                <tr>
+                                    <td>
+                                        <input type="text" className={`form-control m-2 ${styles.tableInput}`} />
+                                    </td>
+                                    <td>
+                                        <input type="number" className={`form-control m-2 ${styles.tableInput}`} />
+                                    </td>
+                                    <td>
+                                        <button type='button' className='btn btn-success m-2'>add</button>
+                                    </td>
+                                </tr>
                                 <tr>
                                     <td>SPayLater</td>
                                     <td>2000</td>
@@ -44,11 +58,11 @@ export default function BoxItem() {
                             </tbody>
                         </table>
                     </div>
-                    <div>
+                    <div className='col-6'>
                         <div>
-                            <span>Expenses</span>
+                            <p>Expenses</p>
                         </div>
-                        <table className='table'>
+                        <table className='table table-dark table-striped'>
                             <thead>
                                 <tr>
                                     <th>Title</th>
@@ -56,7 +70,23 @@ export default function BoxItem() {
                                     <th>Actions</th>
                                 </tr>
                             </thead>
-                            <tbody>
+                            <tbody className=''>
+                                <tr>
+                                    <td>
+                                        <input type="text" className={`form-control m-2 ${styles.tableInput}`} />
+                                    </td>
+                                    <td>
+                                        <input type="number" className={`form-control m-2 ${styles.tableInput}`} />
+                                    </td>
+                                    <td>
+                                        <button type='button' className='btn btn-success m-2'>add</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>SPayLater</td>
+                                    <td>2000</td>
+                                    <td></td>
+                                </tr>
                                 <tr>
                                     <td>SPayLater</td>
                                     <td>2000</td>
